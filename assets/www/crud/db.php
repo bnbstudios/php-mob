ab3b3ca3d50c5694cfcb351ed664b05562e38b3c
bnbstudios <bnbstudios@gmail.com>
2016-07-19 11:47:06 +0545

    Update db.php

--- assets/www/crud/db.php
+++ assets/www/crud/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("192.168.1.74","root","","mobile_db") or die ("could not connect database");
+ $con = mysqli_connect("127.0.0.1","root","","mobile_db") or die ("could not connect database");
 ?>
